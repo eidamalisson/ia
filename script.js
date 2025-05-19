9436728787eaa50861e03f67a143bb6bb10c25ec
eidamalisson <141159381+eidamalisson@users.noreply.github.com>
2025-05-19 10:10:42 -0300

    hhh

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixa principal = document.querySelector
+conts caixa principal = documet
+conts
+const
+const perguntas = [
+{
+enunciado: "pergunta 1",
+alternativas: ["alternativa1","alternativa2"]
+},
+{
+enunciado: "pergunta2"
+alternativa: ["alternativa1","altermativa2"]
+}
+]
